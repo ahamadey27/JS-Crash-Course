10a4576bf30f8bd8efbce33f1d064bcf53faea22
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-04 12:08:57 -0400

    Add string manipulation examples and VSCode settings configuration

--- Ch2-Basics-Misc/strings.js
+++ Ch2-Basics-Misc/strings.js
@@ -1,1 +1,10 @@
-//Getting character from a string 
+//Getting a character from a string
+let alphabet = "ABCDEFG";
+console.log(alphabet[0], alphabet[1]);
+
+//get multiple characters from a string... gets 4th position to 7th
+let sentence = "12345678";
+console.log(sentence.slice(3, 7));
+
+//Trimming white space from a string
+

9e213b068630fc34bc088e7996170543409512b4
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-06 11:21:52 -0400

    Add object literal example for "Casablanca" in objects.js

---
+++ Ch3-Compound-DataTypes/objects.js
@@ -0,0 +1,12 @@
+//Object literal
+let casablanca = {
+    "title": "Casablanca",
+    "released": "1942",
+    "director": "Michael Curtiz"
+}
+console.log(casablanca);
+
+//accessing object value
+console.log(casablanca['title']) 
+console.log(casablanca.director); //use dot notation for values that have valid identifiers
+

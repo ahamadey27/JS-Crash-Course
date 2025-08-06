2c9bcd2c4fb116aba52b92afadaa173206a716ba
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-06 11:40:14 -0400

    Add examples for setting and accessing object values in objects.js

--- Ch3-Compound-DataTypes/objects.js
+++ Ch3-Compound-DataTypes/objects.js
@@ -10,3 +10,15 @@ console.log(casablanca);
 console.log(casablanca['title']) 
 console.log(casablanca.director); //use dot notation for values that have valid identifiers
 
+//Setting object value
+let dictionaryValue = {}
+dictionaryValue.mouse = "small animal";
+dictionaryValue["computer mouse"] = "pointing device";
+dictionaryValue["computer mouse"] = "second device"
+dictionaryValue.mouse = "second small animal"
+console.log(dictionaryValue);
+
+console.log(Object.keys(dictionaryValue)); //get all keys of an object
+console.log(Object.values(dictionaryValue));
+
+

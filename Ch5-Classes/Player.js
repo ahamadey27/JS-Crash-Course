680b41805884fce401843945ecf60408ffa0b9b2
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 18:52:30 -0400

    Enhance comments in Player class for clarity and understanding

--- Ch5-Classes/Player.js
+++ Ch5-Classes/Player.js
@@ -1,9 +1,10 @@
 //Player Class
 class Player 
 {
-    constructor(startX, StartY)
+    //these are methods as they are associated with an object, so no need for the "function" prefix
+    constructor(startX, StartY) //constructor method called automatically anytime object of class is created
     {
-        this.x = startX;
+        this.x = startX; //means "take the value of startX and assign it to the new Player object's x property"
         this.y = StartY;
     }
     move (dx, dy)
@@ -12,3 +13,7 @@ class Player
         this.y += dy;
     }
 }
+
+//Object of Player Class
+//This 1. Creates a new, empty object and 2. The class Constructor is called automatically
+player1 = new Player(0, 0);

8ea6904af046c710009c4ad10ee355ef87a5c95a
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 15:32:02 -0400

    Refactor function expressions and arrow functions for clarity in Functions.js

--- Ch4-Functions/Functions.js
+++ Ch4-Functions/Functions.js
@@ -4,6 +4,7 @@ function sayHi(){
 }
 setTimeout(sayHi, 2);
 
+
 //Function Expressions (function literals) : code literal whose value is a function
 let addExpression = function (x,y)
 {
@@ -11,3 +12,17 @@ let addExpression = function (x,y)
 }
 console.log(addExpression(1, 2));
 
+
+//Arrow functions - compact version of a function expression 
+let addArrow = (x, y) => {
+    return x + y;
+};
+console.log(addArrow(5, 5));
+
+let addArrowConcise = (x, y) => x + y;
+console.log(addArrowConcise(10, 10));
+
+//also good for defining functions that are passed as arguments 
+setInterval(() => {
+    console.log("Beep");
+}, 1000);

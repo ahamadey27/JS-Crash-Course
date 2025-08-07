dd0707642b2dc53bbd04b04c406095c477a323e3
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 16:03:48 -0400

    Add implementation of sum function using rest parameters to calculate total of variable arguments

--- Ch4-Functions/RestParamaters.js
+++ Ch4-Functions/RestParamaters.js
@@ -14,3 +14,15 @@ function myColors(name, ...favoriteColors)
 
 console.log(myColors("hammer", "blue", "red", "orange", "green")); //rest parameter bundles rest of arguments of array arguments 
 
+//Second example...adds all parameters of an unassigned (size) array
+function sum(...numbers)
+{
+    let total = 0;
+    for (let number of numbers)
+    {
+        total +=number;
+    }
+    return total;
+}
+
+console.log(sum(5,10,45,2,64));

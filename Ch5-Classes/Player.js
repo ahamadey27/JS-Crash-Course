6451839449cbe4a791a96d9f7e121ab29f085cf5
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 18:35:15 -0400

    Refactor Player class to ensure proper formatting and structure

--- Ch5-Classes/Player.js
+++ Ch5-Classes/Player.js
@@ -1,1 +1,14 @@
 //Player Class
+class Player 
+{
+    constructor(startX, StartY)
+    {
+        this.x = startX;
+        this.y = StartY;
+    }
+    move (dx, dy)
+    {
+        this.x += dx;
+        this.y += dy;
+    }
+}

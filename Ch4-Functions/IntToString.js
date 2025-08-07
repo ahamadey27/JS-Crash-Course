1e7eaa429c1b8c5543a7c07c477c19e1278badfa
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 15:44:04 -0400

    Add initial implementation of intToString function to convert numbers to strings

---
+++ Ch4-Functions/IntToString.js
@@ -0,0 +1,13 @@
+const numbers = [0, 1, 2, 3, 4, 5];
+const strings = numbers.map(String);
+
+function intToString(index)
+{
+    if (index < 0 || index > numbers.length)
+    {
+        return undefined;
+    }
+    return strings[index];
+}
+
+console.log(intToString(1));

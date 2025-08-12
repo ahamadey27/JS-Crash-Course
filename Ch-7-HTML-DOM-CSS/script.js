8c4a33f1041bb9d1ec81bf626993b6fe7b079e71
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-12 12:34:56 -0400

    Add CSS file and link it in HTML; clean up JavaScript code

--- Ch-7-HTML-DOM-CSS/script.js
+++ Ch-7-HTML-DOM-CSS/script.js
@@ -1,5 +1,5 @@
 let heading = document.getElementById("main-heading");
 let pText = document.getElementById("p-text");
-heading.innerText; //innerText represents text of the element
-heading.innerText = "Hi there";
-pText.innerText = "JS text now"
+
+
+console.log("JS TEST");

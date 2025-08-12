f42f7b92465cf5ddf0a61194cf99ea16f90cc307
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-12 18:14:14 -0400

    Add event handler for paragraph click and improve comments in script.js

--- Ch8-EventBased-Programming/script.js
+++ Ch8-EventBased-Programming/script.js
@@ -1,8 +1,17 @@
 //Adds event handler for when user clicks the h1 element
+//querySelector gets element with main-heading id and save to variable heading
 let heading = document.querySelector("#main-heading");
 
-heading.addEventListener("click", () =>
+//addEventListener tells JS to watch for a particular event happening on the element and execute a function when it does. Has two parameters: first - event type (could also be "keydown" or "scroll"). Second - Function to execute when specified event occurs (aka event handler)
+heading.addEventListener("click", () => //callback function (calling a function within another function)
 {
     console.log("Heading clicked");
     
-})
+});
+
+let paragraph = document.querySelector("par-function");
+paragraph.addEventListener("click", () => 
+{
+    console.log("par clicked!");
+});
+

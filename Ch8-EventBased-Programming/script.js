293456edb0855a1cdc994f56e5ecfbcd702c68d7
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-12 13:10:55 -0400

    Add event handler for main heading click in JavaScript

---
+++ Ch8-EventBased-Programming/script.js
@@ -0,0 +1,8 @@
+//Adds event handler for when user clicks the h1 element
+let heading = document.querySelector("#main-heading");
+
+heading.addEventListener("click", () =>
+{
+    console.log("Heading clicked");
+    
+})

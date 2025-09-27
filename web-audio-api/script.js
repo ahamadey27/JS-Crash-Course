2951e3b9bcf61667e2d7a37e2c327a507d93fe3f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-27 18:57:43 -0400

    Refactor synth loop implementation for consistent note playback timing

--- web-audio-api/script.js
+++ web-audio-api/script.js
@@ -12,19 +12,29 @@ play.addEventListener("click", () => {
     //takes object as an argument
     let synth = new Tone.PolySynth({
         oscillator: {type: "square"},
-        envelope: {attack: 0.8, decay: 0.3, sustain: 0.8, release: 1 }, //values are in seconds 
+        envelope: {attack: 0.0, decay: 0.4, sustain: 0.8, release: 1 }, //values are in seconds 
         volume: -6
     }).toDestination() //connects synth to audio context's output  
 
+    //first argument is function that requires a time value, second is duration indicating how often to call first argument 
+    let loop = new Tone.Loop(time => {
+        synth.triggerAttackRelease("C4", "16n", time);
+    }, "4n");
+
+    loop.start("0:0:0");
+    loop.end("4:0:0")
+
+    Tone.Transport.start();
+
     //parameters are note, duration and time which note is played
-    synth.triggerAttackRelease("A3", 2, 0)
-    synth.triggerAttackRelease(["B3", "D4"], 2, 1) //first argument takes an array for polyphonic notes 
-    synth.triggerAttackRelease("C#4", 2, 2)
-    synth.triggerAttackRelease(["D4", "F#4"], 2, 3)
-    synth.triggerAttackRelease(["E4", "A4"], 2, 4)
-    synth.triggerAttackRelease("F#4", 2, 5)
-    synth.triggerAttackRelease(["G#4", "B4"], 2, 6) 
-    synth.triggerAttackRelease("A4", 2, 7) 
+    // synth.triggerAttackRelease("A3", 2, 0)
+    // synth.triggerAttackRelease(["B3", "D4"], 2, 1) //first argument takes an array for polyphonic notes 
+    // synth.triggerAttackRelease("C#4", 2, 2)
+    // synth.triggerAttackRelease(["D4", "F#4"], 2, 3)
+    // synth.triggerAttackRelease(["E4", "A4"], 2, 4)
+    // synth.triggerAttackRelease("F#4", 2, 5)
+    // synth.triggerAttackRelease(["G#4", "B4"], 2, 6) 
+    // synth.triggerAttackRelease("A4", 2, 7) 
     
     //Basic Audio Web API oscillator with gain 
     /*let audioCtx = new AudioContext();

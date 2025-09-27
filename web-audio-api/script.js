eb12eb6fae2296459662cc0c965224cd951a1571
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-27 19:11:39 -0400

    Fix async handling in play button event and clean up code

--- web-audio-api/script.js
+++ web-audio-api/script.js
@@ -2,12 +2,12 @@
 let play = document.querySelector("#play");
 let playing = document.querySelector("#playing");
 
-play.addEventListener("click", () => {
-    play.style = "display: none"; //Hide this button on click
-    playing.style = ""; //Replaces with "playing" string
+play.addEventListener("click", async () => {
+    play.style.display = "none"; //Hide this button on click
+    playing.style.display = ""; //Replaces with "playing" string
 
     //Synth using Tone.js
-    Tone.start();
+    await Tone.start();
 
     //takes object as an argument
     let synth = new Tone.PolySynth({
@@ -16,39 +16,12 @@ play.addEventListener("click", () => {
         volume: -6
     }).toDestination() //connects synth to audio context's output  
 
-    //first argument is function that requires a time value, second is duration indicating how often to call first argument 
+    // first argument is function that requires a time value, second is duration indicating how often to call first argument
     let loop = new Tone.Loop(time => {
         synth.triggerAttackRelease("C4", "16n", time);
     }, "4n");
 
     loop.start("0:0:0");
-    loop.end("4:0:0")
-
+    loop.stop("4:0:0");
     Tone.Transport.start();
-
-    //parameters are note, duration and time which note is played
-    // synth.triggerAttackRelease("A3", 2, 0)
-    // synth.triggerAttackRelease(["B3", "D4"], 2, 1) //first argument takes an array for polyphonic notes 
-    // synth.triggerAttackRelease("C#4", 2, 2)
-    // synth.triggerAttackRelease(["D4", "F#4"], 2, 3)
-    // synth.triggerAttackRelease(["E4", "A4"], 2, 4)
-    // synth.triggerAttackRelease("F#4", 2, 5)
-    // synth.triggerAttackRelease(["G#4", "B4"], 2, 6) 
-    // synth.triggerAttackRelease("A4", 2, 7) 
-    
-    //Basic Audio Web API oscillator with gain 
-    /*let audioCtx = new AudioContext();
-
-    let oscNode = audioCtx.createOscillator();
-    oscNode.frequency.value = 220;
-
-    let gainNode = audioCtx.createGain();
-    gainNode.gain.value = 0.4;
-
-    oscNode.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-
-    oscNode.start(audioCtx.currentTime + 0.5);
-    oscNode.stop(audioCtx.currentTime + 2);*/
 });
-
